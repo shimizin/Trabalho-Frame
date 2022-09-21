21fede33709eeb0be932fe3904082bd50f00659c
shimizin <65264744+shimizin@users.noreply.github.com>
2022-09-21 08:00:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component{
 
           <div className='flex'>
             <Busca
-              busca={this.state.busca}
+              busca={busca}
               buscaODA={this.buscaODA}
             />
 
